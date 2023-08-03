ac892382416cfa89f2988f83db47aec73585dd00
Sak8dgr8 <bvsaket6000@gmail.com>
2023-08-03 15:44:00 -0400

    signal created to update donation status to completed

---
+++ members/signals.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404
+from paypal.standard.ipn.signals import valid_ipn_received
+from django.dispatch import receiver
+from .models import Donation
+
+
+@receiver(valid_ipn_received)
+def payment_notification(sender, **kwargs):
+    ipn = sender
+    if ipn.payment_status == 'Completed':
+        # payment was successful
+        donation = get_object_or_404(Donation, donation_id=ipn.invoice)
+        donation.status = 'completed'
+        donation.save()
